23dae72dea194c61434f3b8c5e6bbef47f40c9d6
lamb <2374785659@qq.com>
2025-08-01 16:48:11 +0800

    axios拦截器的定义

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,25 @@ import axios from 'axios'
 
 axios.defaults.baseURL = import.meta.env.VITE_VUE_APP_INTERFACE_URL
 
+// axios拦截器定义
+// 请求拦截器
+axios.interceptors.request.use(config => {
+  console.log('请求参数',config)
+  // 请求前可以做些事情，例如设置token
+  return config
+},error => {
+
+  return Promise.reject(error)
+})
+// 响应拦截器
+axios.interceptors.response.use(response => {
+  console.log('响应参数',response)
+  return response
+},error => {
+  return Promise.reject(error)
+})
+
+
 // 引入定义的路由
 import { router } from './router'
 
